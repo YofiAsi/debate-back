5995968c1b227065b4e696b865a99d696ef1931d
Asaf Shilo <71792912+YofiAsi@users.noreply.github.com>
2023-08-31 11:10:33 +0300

    Update server.py

--- server.py
+++ server.py
@@ -943,4 +943,4 @@ bot_room_manager = BotRoomManager()
 eventlet.spawn(bot_room_manager.run)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
+    socketio.run(app)
